5e7fac04ea1a15cd6fe0c9704e58f615475d8d98
Janith Chanaka Abhayarathna <abhayaratnajanith3@gmail.com>
2022-04-15 17:46:45 +0530

    Completed the function of search movie option and fixed a bug.

--- app/src/main/java/com/example/movieapp/SearchMovies.kt
+++ app/src/main/java/com/example/movieapp/SearchMovies.kt
@@ -10,62 +10,72 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
 class SearchMovies : AppCompatActivity() {
-    lateinit var text:TextView
-
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_movies)
 
         val movie = findViewById<EditText>(R.id.movieName)
-        text = findViewById<TextView>(R.id.output)
+        val textOut = findViewById<TextView>(R.id.output)
         val btn = findViewById<Button>(R.id.retreiveBtn)
         val btnSave = findViewById<Button>(R.id.saveMovieBtn)
 
-        btn.setOnClickListener {
+        btn?.setOnClickListener {
 
-            var stb = StringBuilder()
-            val movieName = movie.text.toString()
+            val movieName = movie!!.text.toString().trim()
             val urlString = "https://www.omdbapi.com/?t=$movieName&apikey=ff95b66";
-            val url = URL(urlString)
-            val con: HttpURLConnection = url.openConnection() as HttpURLConnection
 
+            var data: String = ""
             runBlocking {
-                launch {
                     withContext(Dispatchers.IO) {
-                        val bf = BufferedReader(InputStreamReader(con.inputStream))
-                        var line: String? = bf.readLine()
+                        val stb = StringBuilder("")
+
+                        val url = URL(urlString)
+                        val con = url.openConnection() as HttpURLConnection
+                        val bf: BufferedReader
+                        try {
+                            bf = BufferedReader(InputStreamReader(con.inputStream))
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                            return@withContext
+                        }
+                        var line = bf.readLine()
                         while (line != null) {
-                            stb.append(line + "\n")
+                            stb.append(line)
                             line = bf.readLine()
                         }
-                        parseJSON(stb)
+                        data = parseJSON(stb)
+
                     }
-                }
+
             }
+            textOut?.text = data
         }
     }
 
     @SuppressLint("SetTextI18n")
-    suspend fun parseJSON(stb: java.lang.StringBuilder) {
+    suspend fun parseJSON(stb: StringBuilder): String {
 
-        text.text = "Loading..."
-        val jsonObject = JSONObject(stb.toString())
-        val title = jsonObject.getString("Title").toString()
-        val year = jsonObject.getString("Year").toString()
-        val rated = jsonObject.getString("Rated").toString()
-        val released = jsonObject.getString("Released").toString()
-        val runtime = jsonObject.getString("Runtime").toString()
-        val genre = jsonObject.getString("Genre").toString()
-        val director = jsonObject.getString("Director").toString()
-        val writer = jsonObject.getString("Writer").toString()
-        val actors = jsonObject.getString("Actors").toString()
-        val plot = jsonObject.getString("Plot").toString()
-        text.text = "Title: $title\nYear: $year\nRated: $rated\nReleased: $released\nRuntime: $runtime\nGenre: $genre\nDirector: $director\nWriter: $writer\nActors: $actors\nPlot: $plot"
+        var info:String = "bla"
+        val json = JSONObject(stb.toString()) //convert string to JSON object
+        val title = json["Title"].toString()
+        val year = json["Year"].toString()
+        val rated = json["Rated"]
+        val released = json["Released"]
+        val runtime = json["Runtime"]
+        val genre = json["Genre"]
+        val director = json["Director"]
+        val writer = json["Writer"]
+        val actors = json["Actors"]
+        val plot = json["Plot"]
+
+        info = "Title: $title\n, Year: $year\n, Rated: $rated\n, Released: $released\n, Runtime: $runtime, Genre: $genre\n, Director: $director\n, Writer: $writer\n, Actors: $actors\n, Plot: $plot"
+        return info
     }
 }
