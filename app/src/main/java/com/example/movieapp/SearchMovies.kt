ed42d21cf94a51e83d6d8b02f03dea89626fdcc6
Janith Chanaka Abhayarathna <abhayaratnajanith3@gmail.com>
2022-04-15 09:18:01 +0530

    Initialized the search movie window.

---
+++ app/src/main/java/com/example/movieapp/SearchMovies.kt
@@ -0,0 +1,13 @@
+package com.example.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchMovies : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_movies)
+
+
+    }
+}
